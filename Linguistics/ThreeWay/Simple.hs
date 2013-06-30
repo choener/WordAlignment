9748f0d6fb8bee3496de40ccef55fee6e7826408
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-06-30 23:25:55 +0200

    work in progress

--- Linguistics/ThreeWay/Simple.hs
+++ Linguistics/ThreeWay/Simple.hs
@@ -28,3 +28,25 @@ sScore !vowels !consonants !scores !gapOpen = SThreeWay
           {-# INLINE sm #-}
 {-# INLINE sScore #-}
 
+sAlign :: Monad m => SThreeWay m (String,String) (S.Stream m (String,String)) ByteString ()
+sAlign = SThreeWay
+  { loop_loop_step = \(w1,w2,w3) (Z:.():.():.c ) -> (w1++padd "" c  , w2++padd "" c, w3++prnt c "")
+  , loop_step_loop = \(w1,w2,w3) (Z:.():.b :.()) -> (w1++padd "" b  , w2++prnt b "", w3++padd "" b)
+  , loop_step_step = \(w1,w2,w3) (Z:.():.b :.c ) -> (w1++pad2 "" b c, w2++prn2 b c , w3++prn2 c b )
+  , step_loop_loop = \(w1,w2,w3) (Z:.a :.():.()) -> (w1++padd a ""  , 
+  , step_loop_step = \(w1,w2,w3) (Z:.a :.():.c ) -> (w1++prnt a c   , w2++pad2 a c , w3++prnt c a )
+  , step_step_loop = \(w1,w2,w3) (Z:.a :.b :.()) ->
+  , step_step_step = \(w1,w2,w3) (Z:.a :.b :.c ) ->
+  , nil_nil_nil    = const ("","","")
+  , h              = return . id
+  {-
+  { loop_step = \(w1,w2) (Z:.():.c) -> (w1++padd "" c, w2++prnt c "")
+  , step_loop = \(w1,w2) (Z:.c:.()) -> (w1++prnt c "", w2++padd "" c)
+  , step_step = \(w1,w2) (Z:.a:.b) -> (w1++prnt a b,w2++prnt b a)
+  , nil_nil   = const ("","")
+  , h         = return . id
+  -}
+  } where prnt x z = printAligned x [z]
+          padd x z = printAlignedPad '-' x [z]
+{-# INLINE sAlign #-}
+
