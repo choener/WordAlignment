2750ba54deb5029122a72cc562a7a85e291fc19e
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-07-06 23:04:26 +0200

    perfect (?) alignment of aligned characters

--- Linguistics/Common.hs
+++ Linguistics/Common.hs
@@ -28,13 +28,16 @@ printAligned = printAlignedPad ' '
 printAlignedPad :: Char -> ByteString -> [ByteString] -> String
 printAlignedPad p c zs = printf " %s%s" (replicate pad p) (toUtf8String c) where
   pad :: Int
-  pad = maximum $ 0 : map (\x -> printLength x - printLength c) zs
+  pad = (1+) . maximum $ 0 : map (\x -> printLength x - printLength c) zs
 
 -- | Length in /printed characters/ of an UTF8 string wrapped as a 'ByteString'
+--
+-- NOTE 'isMark' selects unicode symbols that modify a character, thereby not
+-- increasing the length of the /printed/ string.
 
 printLength :: ByteString -> Int
 printLength = length . filter isAN . toUtf8String where
-  isAN c = isAlphaNum c || c `elem` [ '\\', '\'', '^', '$', '-', '\'' ]
+  isAN c = not (isMark c) -- isAlphaNum c || c `elem` [ '\\', '\'', '^', '$', '-', '\'' ]
 
 
 {-
